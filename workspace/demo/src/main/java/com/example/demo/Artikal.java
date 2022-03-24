11eea8464515199cf310bf7a96cdc422f026f66e
petarv997 <paja.vidanovic@elfak.rs>
2022-03-24 13:15:35 +0100

    Initial

---
+++ workspace/demo/src/main/java/com/example/demo/Artikal.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class Artikal 
+{
+ private String ime;
+ private double cena;
+ private int skladiste;
+}
